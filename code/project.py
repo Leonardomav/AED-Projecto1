4256dcb3cf8fa51c3760d7857b6b4ee2b61dc53c
jmmonteiro <jmonteiro@student.dei.uc.pt>
2017-03-15 16:57:51 +0000

    commented code

--- code/project.py
+++ code/project.py
@@ -1,13 +1,21 @@
 import csv
 
-csv.register_dialect('AED', delimiter=';')
+csv.register_dialect('AED', delimiter=';') #registers a new dialect, separated with ";", instead of the default ","
 
+#list of lists
 def loadCsvToArray():
-	with open('dados.csv','r') as file:
-		reader = csv.reader(file, dialect='AED')
-		aux=[]
+	#loads the csv file into an array of arrays, 1
+	with open('dados.csv','r') as file: 			#opens the csv file
+		reader = csv.reader(file, dialect='AED')	
+		array = []									#structure to save the good stuff :^)
 		for row in reader:
-			aux.append(row)
-	print(aux)
-	return
+			array.append(row)						#appends a "line" of the '.csv' file, and adds it to the list 'Array'
+	
+	return array 									#returns the 'Array' so you can use it later
 
+#data structure #2
+
+
+
+
+#data structure #3
