681d1e2b3f1079a0dcea7674bc5bc60f207899f3
jmmonteiro <jmonteiro@student.dei.uc.pt>
2017-03-16 19:17:27 +0000

    updating all files

--- code/project.py
+++ code/project.py
@@ -1,18 +1,47 @@
 import monteiro as monteiro
 import leo as leo
 import sopas as sopas
+
 def loadStructures():		#loads the structures to be used in the program
-	struct2 = '@leo'
-	struct3 = '@sopas'
-	return [monteiro.createDict(), struct2, struct3]
+	leonardo = '@leo'
+	s0pas = '@sopas'
+	return [monteiro.createDict(), leonardo, s0pas]		#change later to work properly
 
+def main():
+	mont, leon, sopa = loadStructures()
 
-def menu():
+	#@JMSMonteiro strut needs some more "fine tuning", not necessary if functions are properly made
+	montDict = mont[0]		#dictionary
+	montCountry = mont[1]	#country list
+	montCodes = mont[2]		#country code list
+
+	#@Leonardomav
+
+	#JAfonsoS0pas
+
+	#continue coding
+
 	return
 
+def menuDataStrut():
+	print("Which one of the following data structure you want want to use?")
+	print
+	print("1 - Dictionaries")
+	print("2 - @Leo Strut")
+	print("3 - @Sopas Strut")
+	choice = raw_input(">")
+	#[TODO] protect input ^
+
+	if choice == 1:
+		#Run Function 
+		return
+	elif choice == 2:
+		#Run Function 2
+		return
+	elif choice == 3:
+		#Run Function3
+		return
+
+
 if __name__ == '__main__': 		
-	a1, a2, a3 = loadStructures()	#a1 -> tuple with 1 dict [0] and 2 lists [1] and [2]
-	a1_1 = a1[0]
-	a1_2 = a1[1]
-	a1_3 = a1[2]
-	print(a1_2)
+	main()
