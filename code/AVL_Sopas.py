17c874065cc667fab9cd3189e16f6495a44ed809
JAfonsoS0pas <jafonsopm97@gmail.com>
2017-03-25 19:39:40 +0000

    Add files via upload

--- code/AVL_Sopas.py
+++ code/AVL_Sopas.py
@@ -1,36 +1,70 @@
 import csv
+import time
 from AVL import AVLTreeCountry
 from AVL import AVLTreeData
 
-
-
 csv.register_dialect('AED', delimiter=';') #registers a new dialect, separated with ";", instead of the default ","
 
-arvorePais=AVLTreeCountry()
-dicioPais ={}
-#arvorePais.searchYears(1960)
 
 
+def inputInt(string):
+    while True:
+        try:
+            num = int(input(string))
+        except ValueError:
+            print("Error... Please try again...")
+            continue
+        else:
+            return num
+
+def inputFloat(string):
+    while True:
+        try:
+            num = float(input(string))
+        except ValueError:
+            print("Error... Please try again...")
+            continue
+        else:
+            return num
+
+
+def checkBool(check):
+    if check:
+        print("Done...\n")
+    else:
+        print("Error...\n")
+
+#loads the csv to the AVL tree
 def load():
+	arvorePais=AVLTreeCountry()
 	startingYear=1960
 	with open('dados.csv','r') as file:
 		reader = csv.reader(file ,dialect = 'AED')
+		stopFirst=0
 		for row in reader:
-			years=AVLTreeData()
-			for i in range(len(row)):
-				if row[i] != '' and i!=0 and i!=1:
-					years.insertYears(startingYear+(i-2),row[i])
-			arvorePais.insertCountry(row[0],row[1],years)
+			if stopFirst==0:
+				stopFirst=1
+			else:
+				years=AVLTreeData()
+				for i in range(len(row)):
+					if row[i] != '' and i!=0 and i!=1:
+						years.insertYears(startingYear+(i-2),row[i])
+				arvorePais.insertCountry(row[0],row[1],years)
 	file.close()
+	return arvorePais
 
+#loads the csv to an auxiliar dictionary
 def loadToDict():
+	dicioPais={}
 	with open('dados.csv','r') as file:
 		reader = csv.reader(file,dialect = 'AED')
 		for row in reader:
 			dicioPais[row[0]]=row[1]
+	return dicioPais
 
-
-def tagSearch(tag):
+#Searches the tree by tag with help from the dictionary
+#the function uses the dictionary to get the name that corresponds to the tag
+def tagSearch(tag, arvorePais ,dicioPais):
 	if tag in dicioPais:
 		node=arvorePais.countrySearch(dicioPais.get(tag))
 		print(node)
@@ -38,57 +72,146 @@ def tagSearch(tag):
 		print("Error: Tag does not exist!")
 
 
-
-load()
-loadToDict()
-
-
-
-def dataOfCountry(country):
+#prints all the data of a country (year: XXXX data: XXXX)
+def dataOfCountry(arvorePais):
+	country=str(input("Insert Country: "))
 	node=arvorePais.countrySearch(country)
 	node.getYears().display()
 
-def valuesOfYear(year):
-	tree=arvorePais.searchYears(year)
+#Prints all the Data of all the countries of a given year
+def valuesOfYear(arvorePais):
+	year=inputInt("Insert year: ")
+	arvorePais.searchYears(year)
 
-def searchSpecific(country,year):
+#Prints data of a country of a given year
+def searchSpecific(arvorePais):
+	country=str(input("Insert Country: "))
+	year=inputInt("Insert year: ")
 	node = arvorePais.countrySearch(country)
-	if node.getYears().yearSearch(year) != None:
-		print(node.getYears().yearSearch(year).getData())
+	if node !=False:
+		if node.getYears().yearSearch(year) != None:
+			print(node.getYears().yearSearch(year).getData(),"%")
+	else:
+		print("404 - Country not found!")
 
-def printAll():
-	arvorePais.display()
+#Prints all the information of the tree (countries, years and respective data)
+def printAll(arvorePais):
+	arvorePais.displayAll()
 
-#def getNodeRange(country,min,max)
-#	node = arvorePais.countrySearch(country)
-#	if node.getYears().getValues()
+#Prints the years and values of a country between the limit provided
+def valuesInRange(arvorePais):
+	country = str(input("Insert Country: "))
+	min=inputFloat("Minimum value: ")
+	max=inputFloat("Minimum value: ")
+	node=arvorePais.countrySearch(country)
+	if node != None:
+		node.getYears().getValuesInRange(min,max)
 
-def editYear(country, year, newYear):
+
+#finds a year and changes its year
+def editYear(arvorePais):
+	country = str(input("Insert Country: "))
+	year=inputInt("Choose year: ")
+	newYear=inputInt("Insert New year: ")
 	node = arvorePais.countrySearch(country)
-	if node.getYears().yearSearch(year) != None:
-		node.getYears().yearSearch(year).setYear(newYear)
+	if node != False:
+		if node.getYears().yearSearch(year) != None:
+			oldValue=node.getYears().yearSearch(year).getData()
+			node.getYears().delete(year)
+			node.getYears().insertYears(newYear,oldValue)
+		else:
+			print("404 - Year not found!")
+	else:
+		print("country not found")
 
-def editDara(country,year, newData):
+#finds a year and changes its value
+def editData(arvorePais):
+	country = str(input("Insert Country: "))
+	year = inputInt("Choose year: ")
+	newData = inputInt("Insert New data: ")
 	node = arvorePais.countrySearch(country)
-	if node.getYears().yearSearch(year) != None:
-		node.getYears().yearSearch(year).setData(newData)
+	if node!=False:
+		if node.getYears().yearSearch(year) != None:
+			node.getYears().yearSearch(year).setData(newData)
+		else:
+			print("404 - Year not found!")
+	else:
+		print("country not found")
 
-def deleteCountry(country):
+#deltes a country
+def deleteCountry(arvorePais):
+	country = str(input("Insert Country to delete: "))
 	arvorePais.delete(country)
 
-def deleteYear(country, year):
+#Deletes a year
+def deleteYear(arvorePais):
+	country=str(input("Insert Country to delete: "))
+	year=inputInt("Insert year to delete: ")
 	node = arvorePais.countrySearch(country)
-	if node.getYears().yearSearch(year) != None:
-		node.getYears().delete(year)
+	if node!=False:
+		if node.getYears().yearSearch(year) != None:
+			node.getYears().delete(year)
+	else:
+		print("404 - country not found!")
 
-def addYears(country,year, data):
+#adds a year and data to a country
+def addYears(arvorePais):
+	country = str(input("Insert Country to add: "))
 	node=arvorePais.countrySearch(country)
-	node.insertYears(year,data)
+	year=inputInt("Insert new year: ")
+	data=inputFloat("Insert respective data:  ")
+	if node!=False:
+		node.getYears().insertYears(year,data)
+	else:
+		print("404 - Country not found!")
 
-def addCountry(country, tag, years):
-	arvorePais.insertCountry(country, tag, years)
+#Adds a country to the tree
+def addCountry(arvorePais):
+	country=str(input("Insert Country to add: "))
+	tag=str(input("Insert the tag of the country: "))
+	arvorePais.insertCountry(country, tag, None)
 
-#dataOfCountry("Portugal")
-#valuesOfYear(2000)
-#searchSpecific("Portugal",2000)
-#printAll()
+#prints all the countries and their tags
+def printCountries(arvorePais):
+	arvorePais.displayCountry()
+
+
+#Adds nYears years to a certain country and times the process
+def benchmarkingAddYears(arvorePais):
+    nYears = inputInt("\nNumber of years to add:\n>")
+    start = time.time()
+    yearsTree = arvorePais.countrySearch("Portugal").getYears()
+    for i in range(nYears):
+        yearsTree.insertYears(i, i)
+    end = time.time()
+    print('Done in ' + str(end - start),'seconds...')
+	#deltes the added values
+    benchmarkingRemoveYears(arvorePais, yearsTree, nYears)
+
+#Removes nYears years and times the process
+def benchmarkingRemoveYears(arvorePais, yearsTree, nYears):
+    start = time.time()
+    for i in range(nYears):
+        yearsTree.delete(i)
+
+    end = time.time()
+    print('[REMOVE] - Done in ' + str(end - start) + ' seconds...')
+
+#Adds nCountries countries to the tree and times the process
+def benchmarkingAddCountries(arvorePais):
+    nCountries = inputInt("\nNumber of years to add:\n>")
+    start = time.time()
+    for i in range(nCountries):
+        arvorePais.insertCountry([str(i), str(i)])
+    end = time.time()
+    print('Added all the countries in ' + str(end - start) + ' seconds...')
+	#removes the added countries
+    benchmarkingRemoveCountries(arvorePais, nCountries)
+
+#Removes nCountries countries and times the process
+def benchmarkingRemoveCountries(arvorePais, nCountries):
+    start = time.time()
+    for i in range(nCountries):
+        arvorePais.delete(str(i))
+    end = time.time()
+    print('Removed all the coutries in ' + str(end - start) + ' seconds...')
--- code/project.py
+++ code/project.py
@@ -1,7 +1,7 @@
-import time
-import monteiro as monteiro
+#import monteiro as monteiro
 import doubleLinkedLists as dll
-import sopas as sopas
+import AVL_Sopas as AVL
+
 
 def inputInt(string):
     while True:
@@ -13,198 +13,188 @@ def inputInt(string):
         else:
             return num
 
-def loadStructures(): #loads the structures to be used in the program
-	DoubleLinkedLists = dll.loadCsvToArray()
-	monteiro = "Stacks" #chage this for the load
-	sopas = "AVL" #chage this for the load
 
-	return [monteiro, DoubleLinkedLists, sopas]
+def loadStructures():  # loads the structures to be used in the program
+    DoubleLinkedLists = dll.loadCsvToArray()
+    monteiro = "Stacks"  # chage this for the load
+    countriesAVL = AVL.load() # chage this for the load
+    tagDic= AVL.loadToDict()
+
+    return [monteiro, DoubleLinkedLists, countriesAVL, tagDic]
+
 
 def menuDataStrut():
+    while True:
+        print("\nWhich one of the following data structure you want want to use?")
+        print("1 - Stack of Stacks")
+        print("2 - DoubleLinkedList of DoubleLinkedLists")
+        print("3 - AVL tree of AVL trees")
+        print("0 - Quit")
+        structPicked = inputInt("> ")
+        if structPicked == 0 or structPicked == 1 or structPicked == 2 or structPicked == 3:
+            break
+        else:
+            print("Option not available... Try Again...\n")
 
-	while True:
-		print("\nWhich one of the following data structure you want want to use?")
-		print("1 - Stack of Stacks")
-		print("2 - DoubleLinkedList of DoubleLinkedLists")
-		print("3 - AVL tree of AVL trees")
-		print("0 - Quit")
-		structPicked= inputInt("> ")
-		if structPicked == 0 or structPicked == 1 or structPicked == 2 or structPicked == 3:
-			break
-		else:
-			print("Option not available... Try Again...\n")
-	
-	return structPicked
-
-def menu(countriesStack, countriesList, countriesAVL, structPicked):
-
-	while True:
-		print("\n---------------------------------------------------\nMENU")
-		print("1  - Search all the years with available information of one country;")
-		print("2  - Search all the countries information of one year;")
-		print("3  - Search one year information of one country;")
-		print("4  - Search all the years of all the countries;")
-		print("5  - Search a range of percentages of one country;")
-		print("6  - Edit year of a country;")
-		print("7  - Edit year's respective percentage of a country;")
-		print("8  - Remove one country;")
-		print("9  - Remove one year of one country;")
-		print("10 - Add one country;")
-		print("11 - Add one pair [Year, Percentage] to a country;")
-		print("12 - BENCHMARKING - Add 'x' years;")
-		print("13 - BENCHMARKING - Add and remove 'x' countries;")
-		print("0  - Quit")
-		choice = inputInt("> ")
+    return structPicked
 
 
-		#delete the "continues"
-		if choice == 1:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.allYearsFromCountry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+def menu(countriesStack, countriesList, countriesAVL, structPicked, tagDic):
+    while True:
+        print("\n---------------------------------------------------\nMENU")
+        print("1  - Search all the years with available information of one country;")
+        print("2  - Search all the countries information of one year;")
+        print("3  - Search one year information of one country;")
+        print("4  - Search all the years of all the countries;")
+        print("5  - Search a range of percentages of one country;")
+        print("6  - Edit year of a country;")
+        print("7  - Edit year's respective percentage of a country;")
+        print("8  - Remove one country;")
+        print("9  - Remove one year of one country;")
+        print("10 - Add one country;")
+        print("11 - Add one pair [Year, Percentage] to a country;")
+        print("12 - BENCHMARKING - Add 'x' years;")
+        print("13 - BENCHMARKING - Add and remove 'x' countries;")
+        print("0  - Quit")
+        choice = inputInt("> ")
 
-		elif choice == 2:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.allCountryFromYear(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        # delete the "continues"
+        if choice == 1:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.allYearsFromCountry(countriesList)
+            elif structPicked == 3:
+                AVL.dataOfCountry(countriesAVL)
 
-		elif choice == 3:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.oneYearFromOneCoutry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 2:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.allCountryFromYear(countriesList)
+            elif structPicked == 3:
+                AVL.valuesOfYear(countriesAVL)
 
-		elif choice == 4:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.allYearsFromAllCountries(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
 
-		elif choice == 5:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.RangeOfDataOfOneCountry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 3:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.oneYearFromOneCoutry(countriesList)
+            elif structPicked == 3:
+                AVL.searchSpecific(countriesAVL)
 
-		elif choice == 6:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.editYearOfACountry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 4:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.allYearsFromAllCountries(countriesList)
+            elif structPicked == 3:
+                AVL.printAll(countriesAVL)
 
-		elif choice == 7:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.editValueOfAYear(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 5:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.RangeOfDataOfOneCountry(countriesList)
+            elif structPicked == 3:
+                AVL.valuesInRange(countriesAVL)
 
-		elif choice == 8:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.removeCountry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 6:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.editYearOfACountry(countriesList)
+            elif structPicked == 3:
+                AVL.editYear(countriesAVL)
+                countriesAVL.rebalance()
 
-		elif choice == 9:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.removeYearFromCountry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 7:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.editValueOfAYear(countriesList)
+            elif structPicked == 3:
+                AVL.editData(countriesAVL)
 
-		elif choice == 10:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.addCountry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 8:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.removeCountry(countriesList)
+            elif structPicked == 3:
+                AVL.deleteCountry(countriesAVL)
 
-		elif choice == 11:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.addYearToCountry(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 9:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.removeYearFromCountry(countriesList)
+            elif structPicked == 3:
+                AVL.deleteYear(countriesAVL)
 
-		elif choice == 12:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.benchmarkingAddYearsMiddle(countriesList)
-				dll.benchmarkingAddYearsStart(countriesList)
-				dll.benchmarkingAddYearsEnd(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 10:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.addCountry(countriesList)
+            elif structPicked == 3:
+                AVL.addCountry(countriesAVL)
 
-		elif choice == 13:
-			if structPicked == 1:
-				continue
-				#@SOPAS STRUT FUNCTION HERE
-			elif structPicked == 2:
-				dll.benchmarkingAddCountries(countriesList)
-			elif structPicked == 3:
-				continue
-				#@MONTEIRO STRUT FUNCTION HERE
+        elif choice == 11:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.addYearToCountry(countriesList)
+            elif structPicked == 3:
+                AVL.addYears(countriesAVL)
 
-		elif choice == 0:
-			break
+        elif choice == 12:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.benchmarkingAddYearsMiddle(countriesList)
+                dll.benchmarkingAddYearsStart(countriesList)
+                dll.benchmarkingAddYearsEnd(countriesList)
+            elif structPicked == 3:
+                AVL.benchmarkingAddYears(countriesAVL)
+        elif choice == 13:
+            if structPicked == 1:
+                continue
+            # @SOPAS STRUT FUNCTION HERE
+            elif structPicked == 2:
+                dll.benchmarkingAddCountries(countriesList)
+            elif structPicked == 3:
+                AVL.benchmarkingAddCountries(countriesAVL)
 
-		else:
-			print("Option not available... Try Again...\n")
+        elif choice == 0:
+            break
+
+        else:
+            print("Option not available... Try Again...\n")
 
 
 def main():
-	countriesStack, countriesList, countriesAVL = loadStructures()
+    countriesStack, countriesList, countriesAVL, tagDic = loadStructures()
 
-	while True:
-		structPicked=menuDataStrut()
-		if structPicked == 0:
-			break
-		menu(countriesStack, countriesList, countriesAVL, structPicked)
+    while True:
+        structPicked = menuDataStrut()
+        if structPicked == 0:
+            break
+        menu(countriesStack, countriesList, countriesAVL, structPicked, tagDic)
 
-if __name__ == '__main__': 		
-	main()
+
+if __name__ == '__main__':
+    main()
